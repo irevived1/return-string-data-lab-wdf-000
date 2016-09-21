b9c9253f96a21237d2e2ac9de3b7d4e17d0b8166
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-21 15:12:38 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,35 @@
 class ProductsController < ApplicationController
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Good job."
+      redirect_to products_path
+    else
+      flash[:notice] = "Try again."
+      render 'new'
+    end
+  end
 
+  def inventory
+    @product = Product.find_by(id:params[:id])
+    if @product && @product.inventory > 0
+      return render plain: "true"
+    end
+    return render plain: "false"
+  end
+
+  def description
+    @product = Product.find_by(id:params[:id])
+    if @product && @product.description
+      return render plain: @product.description
+    end
+    return render plain: "blank"
+  end
+
+
+  private
+  
+  def product_params
+    params.require(:product).permit(:name,:price,:inventory,:description)
+  end
 end
